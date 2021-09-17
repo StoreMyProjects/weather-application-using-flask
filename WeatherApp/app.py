4e4bf6bd0bc8650aa0f4aca16dc137ee2dd53ac7
amrendra01 <singhamrendra1999@gmail.com>
2021-09-17 09:46:10 +0530

    added cloudiness percentage feature

--- WeatherApp/app.py
+++ WeatherApp/app.py
@@ -8,19 +8,19 @@ app = Flask(__name__)
 def index():
 
     if request.method == "POST":
-        city = request.form['city']
-        country = request.form['country']
-        api_key = "Your_API_key"
+        city_name = request.form['city']
+        api_key = "your_api_key"
 
-        weather_url = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={city},{country}&appid={api_key}&units=metric')
+        weather_url = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=metric')
 
         weather_data = weather_url.json()
 
-        temp = weather_data['main']['temp']
+        temp = round(weather_data['main']['temp'])
         humidity = weather_data['main']['humidity']
         wind_speed = weather_data['wind']['speed']
+        clouds = weather_data['clouds']['all']
 
-        return render_template("result.html", temp=temp, humidity=humidity, wind_speed=wind_speed, city=city)
+        return render_template("result.html", temp=temp, humidity=humidity, wind_speed=wind_speed, city=city_name)
 
     return render_template("layout.html")
 

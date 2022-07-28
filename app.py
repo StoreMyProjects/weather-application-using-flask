ff9f2bc3fe55f1b710565067ef972cd238f23167
amrendra01 <singhamrendra1999@gmail.com>
2022-07-28 13:39:07 +0530

    weather icon code added

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import requests
 import json
+# from pprint import pprint
 
 app = Flask(__name__)
 
@@ -9,19 +10,21 @@ def index():
 
     if request.method == "POST":
         city_name = request.form['city']
-        api_key = 'your_api_key'
+        api_key = "WEATHER_API_KEY"
 
         weather_url = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=metric')
 
         weather_data = weather_url.json()
+        # pprint(weather_data)
 
         temp = round(weather_data['main']['temp'])
+        icon = weather_data["weather"][0]["icon"]
         weather = weather_data["weather"][0]["main"]
         humidity = weather_data['main']['humidity']
         wind_speed = weather_data['wind']['speed']
         cloudiness = weather_data['clouds']['all']
 
-        return render_template("result.html", temp=temp, humidity=humidity, wind_speed=wind_speed, cloudiness=cloudiness, weather=weather, city=city_name)
+        return render_template("result.html", temp=temp, humidity=humidity, wind_speed=wind_speed, cloudiness=cloudiness, weather=weather, icon = icon, city=city_name)
 
     return render_template("index.html")
 
--- test.py
+++ test.py
@@ -3,8 +3,9 @@ import json
 
 city_name = "Nashik"
 
-api_key = "your_api_key"
+api_key = "WEATHER_API_KEY"
 
 weather_url = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=metric')
 
 weather_data = weather_url.json()
+print(weather_url.status_code)
